4f46efc5a62a1964f92aa002ca711f1cb4ecfa56
Paul Schoolden <paul.schoolden@sky.com>
2018-02-23 16:04:27 +0000

    Updated to use MVVM Light

--- Source/ManicStreetCoder.AzureDashform.Tests/ViewModel/MainTests.cs
+++ Source/ManicStreetCoder.AzureDashform.Tests/ViewModel/MainTests.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ManicStreetCoder.AzureDashform.Tests.ViewModel
+﻿namespace ManicStreetCoder.AzureDashform.Tests.ViewModel
 {
-    class MainTests
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class MainTests
     {
     }
 }
