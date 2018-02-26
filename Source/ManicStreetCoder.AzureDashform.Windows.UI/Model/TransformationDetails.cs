f5bb85bcbc23a6b3e0de024d35d84e7de561b316
Paul Schoolden <paul.schoolden@sky.com>
2018-02-26 17:52:24 +0000

    Added model

---
+++ Source/ManicStreetCoder.AzureDashform.Windows.UI/Model/TransformationDetails.cs
@@ -0,0 +1,18 @@
+﻿namespace ManicStreetCoder.AzureDashform.Windows.UI.Model
+{
+    public class TransformationDetails
+    {
+        public TransformationDetails()
+        {
+        }
+
+        public TransformationDetails(string sourceFilePath)
+        {
+            this.SourceFilePath = sourceFilePath;
+        }
+
+        public string SourceFilePath { get; set; }
+
+        public string OutputFilePath { get; set; }
+    }
+}
--- Source/ManicStreetCoder.AzureDashform.Windows.UI/ViewModel/MainViewModel.cs
+++ Source/ManicStreetCoder.AzureDashform.Windows.UI/ViewModel/MainViewModel.cs
@@ -2,15 +2,15 @@ using GalaSoft.MvvmLight;
 
 namespace ManicStreetCoder.AzureDashform.ViewModel
 {
+    using AzureDashform.Windows.UI.Model;
+
     public class MainViewModel : ViewModelBase
     {
         public MainViewModel()
         {
-            this.SourceFilePath = @"C:\Temp\Summmit.json";
+            this.Details = new TransformationDetails(@"C:\Temp\Summmit.json");
         }
 
-        public string SourceFilePath { get; set; }
-
-        public string OutputFilePath { get; set; }
+        public TransformationDetails Details { get; set; }
     }
 }
